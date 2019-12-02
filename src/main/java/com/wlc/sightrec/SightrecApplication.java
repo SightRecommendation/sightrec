206182ec07dd3a239af24edc75f97bae36afb96d
chusiyuan <chusiyuan96@gmail.com>
2019-12-02 17:48:36 +0800

    initialize

---
+++ src/main/java/com/wlc/sightrec/SightrecApplication.java
@@ -0,0 +1,13 @@
+package com.wlc.sightrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SightrecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SightrecApplication.class, args);
+    }
+
+}
