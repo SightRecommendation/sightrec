ea0ce0e60a686da97a25a1d9fc9b263a6b7c676b
JingqingLin <JingqingLin@live.com>
2020-02-28 01:12:28 +0800

    Update Comments.vue

--- sightrec-admin-vue/src/components/comment/Comments.vue
+++ sightrec-admin-vue/src/components/comment/Comments.vue
@@ -257,7 +257,7 @@
       async removeCommentById(id) {
         // 弹框询问用户是否删除数据
         const confirmResult = await this.$confirm(
-          '此操作将永久删除该用户, 是否继续？',
+          '此操作将删除该评论, 是否继续？',
           '提示',
           {
             confirmButtonText: '确定',
