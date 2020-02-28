50b36ed5f3df39d0ba87a95176f48a4a1d10077c
JingqingLin <JingqingLin@live.com>
2020-02-29 02:08:17 +0800

    Update Login.vue

--- sightrec-admin-vue/src/components/Login.vue
+++ sightrec-admin-vue/src/components/Login.vue
@@ -4,8 +4,8 @@
       <!--      登陆表单区域-->
       <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
         <!--        用户名-->
-        <el-form-item prop="username">
-          <el-input  v-model="loginForm.username" prefix-icon="el-icon-user"></el-input>
+        <el-form-item prop="name">
+          <el-input  v-model="loginForm.name" prefix-icon="el-icon-user"></el-input>
         </el-form-item>
         <!--        密码-->
         <el-form-item prop="password">
@@ -26,11 +26,11 @@ export default {
   data () {
     return {
       loginForm: {
-        username: 'admin',
+        name: 'admin',
         password: '123456'
       },
       loginFormRules: {
-        username: [{ required: true, message: '请输入用户名', trigger: 'blur' },
+        name: [{ required: true, message: '请输入用户名', trigger: 'blur' },
           { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }],
         password: [{ required: true, message: '请输入密码', trigger: 'blur' },
           { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }]
@@ -44,9 +44,10 @@ export default {
     login () {
       this.$refs.loginFormRef.validate(async valid => {
         if (!valid) return
-        const { data: result } = await this.$http.post('login', this.loginForm)
-        if (result.meta.status !== 200) this.$message.error('登陆失败')
-        else this.$message.success('登陆成功')
+        const { data: result } = await this.$http.post('/admins/login', this.loginForm)
+        // 登录请求返回的 Json 中没有 status 属性，先注释
+        // if (result.meta.status !== 200) this.$message.error('登陆失败')
+        // else this.$message.success('登陆成功')
         // console.log(result)
         window.sessionStorage.setItem('token', result.data.token)
         this.$router.push('/home')
