6d15665a0fdbc91619d9ca9a32c61aa448c89360
JingqingLin <JingqingLin@live.com>
2020-02-29 01:51:30 +0800

    同步请求端口

--- sightrec-admin-vue/src/main.js
+++ sightrec-admin-vue/src/main.js
@@ -9,7 +9,7 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 // 配置请求的跟路径
-axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
 axios.interceptors.request.use(config => {
   // console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
