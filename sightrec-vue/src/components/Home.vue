5326951d5f8c82070bf3ca97ecfd6b31890bb19c
chusiyuan <chusiyuan96@gmail.com>
2020-04-23 15:31:15 +0800

    add home.header

--- sightrec-vue/src/components/Home.vue
+++ sightrec-vue/src/components/Home.vue
@@ -1,8 +1,19 @@
 <template>
-  <div id="home">
-    hh
-    <router-view></router-view>
-  </div>
+  <el-scrollbar class="home-scrollbar">
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+      <el-header>
+        <div>
+          <img src="https://cdn.jsdelivr.net/gh/JingqingLin/ImageHosting/img/592aef2edd2d3.png" width="48px" />
+          <span>gogo</span>
+        </div>
+        <el-input placeholder="请输入内容" clearable>
+          <el-button slot="append" icon="el-icon-search" ></el-button>
+        </el-input>
+        <el-button type="danger" size="small" @click="logout" plain>退出</el-button>
+      </el-header>
+    </el-container>
+  </el-scrollbar>
 </template>
 
 <script>
@@ -12,5 +23,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #303133;
+    font-size: 20px;
+    border: 0px solid #dcdfe6;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 0 6px;
+    backdrop-filter: blur(6px);
+    background: rgba(255, 255, 255, 0.7);
+    z-index: 2;
+    position: fixed;
+    top: 0;
+    width: 100%;
+
+    >div {
+      margin-left: 10px;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 10px;
+      }
+    }
+
+    .el-input {
+      margin-left: 150px;
+      margin-right: 700px;
+    }
+  }
 </style>
--- sightrec-vue/src/plugins/element.js
+++ sightrec-vue/src/plugins/element.js
@@ -1,4 +1,69 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  InputNumber,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option,
+  DatePicker,
+  Scrollbar,
+  Link
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(DatePicker)
+Vue.use(Scrollbar)
+Vue.use(Link)
+
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
